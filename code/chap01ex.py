e06b936f4089eb0f73886394fd305fe72207a40c
Agata Redmerska <agataredmerska@gmail.com>
2020-07-15 15:21:09 +0200

    chapter01 exercise2 - done

--- code/chap01ex.py
+++ code/chap01ex.py
@@ -14,6 +14,56 @@ import nsfg
 import thinkstats2
 
 
+def ReadFemResp(dct_file='2002FemResp.dct',
+                dat_file='2002FemResp.dat.gz',
+                nrows=None):
+    """Reads the NSFG respondent data.
+    
+    dct_file: string file name
+    dat_file: string file name
+
+    returns: DataFrame
+    """
+    dct = thinkstats2.ReadStataDct(dct_file)
+    df = dct.ReadFixedWidth(dat_file, compression='gzip', nrows=nrows)
+    CleanFemResp(df)
+    return df
+
+
+def CleanFemResp(df):
+        """Recodes variables from the respondent frame.
+        
+        df: DataFrame
+        """
+        pass
+    
+
+def ValidatePregnum(resp):
+    """Validate pregnum in the respondent file.
+    
+    resp: respondent DataFrame
+    """
+    #read the pregnancy frame
+    preg = nsfg.ReadFemPreg()
+    
+    #make the map form caseid to list of pregnancy indices
+    preg_map = nsfg.MakePregMap(preg)
+    
+    #iterate through the respondent pregnum series
+    for index, pregnum in resp.pregnum.items():
+        caseid = resp.caseid[index]
+        indices = preg_map[caseid]
+        
+        #check that pregnum form the respondent file equals
+        #the number of records in the pregnancy file
+        if len(indices) != pregnum:
+            print(caseid, len(indices), pregnum)
+            return False
+        
+    return True
+        
+        
+        
 def main(script):
     """Tests the functions in this module.
 
